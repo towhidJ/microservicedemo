822d89ddd190fae9f89e33c59a971bb0ac1a4ad4
towhidJ <towhidulislamcse@gmail.com>
2023-09-09 11:28:00 +0600

    add catalog

---
+++ src/Services/Catalog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Context;
+
+public class CatalogDbContext:ApplicationDbContext
+{
+    private static IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+        .Build();
+    public static string? connectionString = configuration.GetConnectionString("Catalog.API");
+    public static string? databaseName = configuration.GetValue<string>("DatabaseName");
+
+    public CatalogDbContext() : base(connectionString, databaseName)
+    {
+
+    }
+}
---
+++ src/Services/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Catalog.API.Interfaces.Manager;
+using Catalog.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private IProductManager _productManager;
+
+        public CatalogController(IProductManager productManager)
+        {
+            _productManager = productManager;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)]
+        public IActionResult GetProduct()
+        {
+            var products = _productManager.GetAll();
+            return Ok(products);
+        }
+    }
+}
